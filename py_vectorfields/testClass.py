814247a0a4b61c11d9deaa58e5716a0daa18a3af
Anna Shtengel <stigersh@gmail.com>
2023-05-05 20:21:15 +0200

    mesh for failing normal calculation with libigl and openmp

--- py_vectorfields/testClass.py
+++ py_vectorfields/testClass.py
@@ -1,16 +1,18 @@
 #%%
 import numpy as np
 import scipy
-from build import py_vectorfields
+
 import sys
 import igl
 import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 #%%
+from build import py_vectorfields
 sys.path.append('../../')
 
 #%%
 
-shape_name = 'torus_mid_g1.off'
+shape_name = "lock_input_tri_g3.obj"#'torus_mid_g1.off'
 MESH_DIR ="./example_data/"
 v,f = igl.read_triangle_mesh(os.path.join(MESH_DIR, shape_name))
 
